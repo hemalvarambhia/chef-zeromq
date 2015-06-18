50aee23dc2f544909207aab06ed611167a28092c
Hemal <h.n.varambhia@gmail.com>
2015-06-18 15:45:38 +0100

    The shared object is stored elsewhere in Ubuntu 14.04

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe "Installing ZeroMQ" do
- describe file "/usr/lib/libzmq.so" do
+ lib_path = if os[:release].to_f == 14.04
+ 	      "/usr/local/lib"
+	    else
+	      "/usr/lib"
+ 	    end
+
+ describe file "#{lib_path}/libzmq.so" do
    it { should be_file }
  end
 
- describe command "ldconfig -p | grep /usr/lib/libzmq.so" do
+ describe command "ldconfig -p | grep #{lib_path}/libzmq.so" do
    its(:stdout) { should match /libzmq.so/ } 
  end
 end
