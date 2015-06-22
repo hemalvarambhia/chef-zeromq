c44f04630be219d014d03375511b4e086c465a21
Hemal <h.n.varambhia@gmail.com>
2015-06-22 11:07:36 +0100

    Use the version specified in the attributes script

--- libraries/chef_zerommq_helper.rb
+++ libraries/chef_zerommq_helper.rb
@@ -2,7 +2,7 @@ module ChefZeroMQ
   module Helper
     include Chef::Mixin::ShellOut
     def already_unpacked?
-      install_path = "/usr/local/src/zeromq-2.2.0"
+      install_path = "/usr/local/src/zeromq-#{node[:zeromq][:version]}"
       dir_extracted = shell_out("[ -d #{install_path} ] && echo exists", returns: [0, 2])
       
       dir_extracted.stderr.empty? && dir_extracted.stdout.strip == "exists"      
