dfd95911b05791f633ad98d9aaf496d73917f416
Hemal <h.n.varambhia@gmail.com>
2015-06-22 11:11:51 +0100

    Use the node attribute

--- libraries/chef_zerommq_helper.rb
+++ libraries/chef_zerommq_helper.rb
@@ -2,7 +2,7 @@ module ChefZeroMQ
   module Helper
     include Chef::Mixin::ShellOut
     def already_unpacked?
-      install_path = "/usr/local/src/zeromq-#{node[:zeromq][:version]}"
+      install_path = "#{node[:zeromq][:install_dir]}/zeromq-#{node[:zeromq][:version]}"
       dir_extracted = shell_out("[ -d #{install_path} ] && echo exists", returns: [0, 2])
       
       dir_extracted.stderr.empty? && dir_extracted.stdout.strip == "exists"      
